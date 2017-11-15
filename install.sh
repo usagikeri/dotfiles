b192290f8ee323f457ef228c89e9e3997dd631ea
Usagikeri <candidandoras@gmail.com>
2017-11-15 17:56:14 +0900

    rename install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 PWD=`pwd`
 
-ln -sf $PWD/.zshrc ~/.zshrc
-ln -sf $PWD/.tmux.conf ~/.tmux.conf
-ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/zshrc ~/.zshrc
+ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/vimrc ~/.vimrc
