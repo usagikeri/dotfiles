595c170efc037c616f5358b3ab3b349ed0c01972
Usagikeri <a.k.a.usagikeri@gmail.com>
2019-01-18 12:13:49 +0900

    add meomo

--- setup.sh
+++ setup.sh
@@ -29,5 +29,14 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 pyenv install 3.7.0
 
-# Neovim PlugInstall
-nvim +PlugInstall +qa
+# Neovim pluginstall
+nvim +pluginstall +qa
+
+# ============================================================================================== #
+# スクリプト実行後に行うこと                                                                     #
+# ============================================================================================== #
+# - texは`brew cask install mactex`を行ってもなぜかplatexコマンドが使えないので，                #
+#   pkgをWebからダウロードして，インストールする．                                               #
+# - .commadファイルをalfredから呼び出したときに自動的に終了するように，                          #
+#   ターミナルの `環境設定/シェル/シェル終了時:` を `シェルが正常に終了した場合は閉じる` にする．#
+# ============================================================================================== #
